76fcd3b076bff9f8a4acac22f85f4e4535f3b5fe
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2024-05-27 19:43:45 +0700

    Update docker

--- auth/server.js
+++ auth/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 app.use('/auth', authRoutes);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Auth service running on port ${PORT}`);
 });
