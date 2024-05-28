a24cd0daa68d5b2ec66aae11f0d3c1ff9322eeb2
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-05-28 08:57:36 +0700

    create branch authen

--- crud/db.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize("tcc", "rian", "ribud123", {
-  host: process.env.DB_HOST,
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
